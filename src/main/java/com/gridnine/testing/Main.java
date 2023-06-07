df8e541f91d88cfe29f0cdfab774cf46b1e78fa1
SkobelevaLuda <skobeleva.ludmila@yandex.ru>
2023-06-07 12:21:25 +0300

    Two commit

---
+++ src/main/java/com/gridnine/testing/Main.java
@@ -0,0 +1,22 @@
+package com.gridnine.testing;
+
+import com.gridnine.testing.filter.FlightFilters;
+import com.gridnine.testing.flight.Flight;
+import com.gridnine.testing.flight.FlightBuilder;
+import com.gridnine.testing.util.Log;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        try {
+            List<Flight> flights = FlightBuilder.createFlights();
+
+            flights.stream().filter(FlightFilters::check)
+                    .forEach(flight -> Log.info(flight.toString()));
+        } catch (Exception e) {
+            Log.error(e);
+        }
+    }
+}
---
+++ src/main/java/com/gridnine/testing/filterImpl/DepartingInPast.java
@@ -0,0 +1,19 @@
+package com.gridnine.testing.filterImpl;
+
+import com.gridnine.testing.filter.FlightFilter;
+import com.gridnine.testing.flight.Flight;
+
+import javax.swing.text.Segment;
+import java.time.LocalDateTime;
+
+public class DepartingInPast implements FlightFilter {
+    @Override
+    public boolean check(Flight flight) {
+        for (Segment seg : flight.getSegments()) {
+            if (seg.getDepartureDate().isBefore(LocalDateTime.now())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
